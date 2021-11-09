eda2784a413b96b03aedd8c226bf87ba3c597d50
tractortoby <tobychung1993@gmail.com>
2021-11-09 15:13:45 +0800

    rewrite App.js with React Hooks, problem: orange button like +,-, *,  ÷on't work

--- src/component/App.js
+++ src/component/App.js
@@ -1,26 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Display from "./Display";
 import ButtonPanel from "./ButtonPanel";
 import calculate from "../logic/calculate";
 import "./App.css";
 
-export default class App extends React.Component {
-  state = {
+// export default class App extends React.Component {
+//   state = {
+//     total: null,
+//     next: null,
+//     operation: null,
+//   };
+
+//   handleClick = buttonName => {
+//     this.setState(calculate(this.state, buttonName));
+//   };
+
+//   render() {
+//     return (
+//       <div className="component-app">
+//         <Display value={this.state.next || this.state.total || "0"} />
+//         <ButtonPanel clickHandler={this.handleClick} />
+//       </div>
+//     );
+//   }
+// }
+
+const App = () => {
+  const [state, setState] = useState({
     total: null,
     next: null,
     operation: null,
+  });
+
+  // useEffect(() => {
+  //   function handleClick(buttonName) {
+  //     setState(calculate(state, buttonName));
+  //   };
+  // });
+
+  const handleClick = (buttonName) => {
+    setState(calculate(state, buttonName));
   };
 
-  handleClick = buttonName => {
-    this.setState(calculate(this.state, buttonName));
-  };
+  return (
+    <div className="component-app">
+      <Display value={state.next || state.total || "0"} />
+      <ButtonPanel clickHandler={handleClick} />
+    </div>
+  );
+};
 
-  render() {
-    return (
-      <div className="component-app">
-        <Display value={this.state.next || this.state.total || "0"} />
-        <ButtonPanel clickHandler={this.handleClick} />
-      </div>
-    );
-  }
-}
+export default App;
