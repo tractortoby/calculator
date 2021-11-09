37f5e22aca4a0c5dcabdaa3efb0186ed96e04c50
tractortoby <tobychung1993@gmail.com>
2021-11-09 14:16:21 +0800

    Updates

---
+++ precache-manifest.6e1147cdf58b4b0b1700af2c64a45d1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63fffce7a3c0e18425200119739dda44",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "58e3861956b6dcf89c5b",
+    "url": "/calculator/static/css/2.f87fbcaa.chunk.css"
+  },
+  {
+    "revision": "ef2e1474b08139829005",
+    "url": "/calculator/static/css/main.37131298.chunk.css"
+  },
+  {
+    "revision": "58e3861956b6dcf89c5b",
+    "url": "/calculator/static/js/2.9067ee69.chunk.js"
+  },
+  {
+    "revision": "ef2e1474b08139829005",
+    "url": "/calculator/static/js/main.5bfd6437.chunk.js"
+  },
+  {
+    "revision": "2fe8cbdbbea1fbf116e6",
+    "url": "/calculator/static/js/runtime~main.2539f434.js"
+  }
+]);
